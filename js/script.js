cbe88bdca08e84570b6b7901ee240dc8592bbf0f
Youss0203 <youssefhalibi003@gmail.com>
2023-11-10 15:45:21 +0100

    terza commit

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ const numeroChilometri = parseInt(prompt("quanti chilometri vuoi percorrere"));
 const etàPasseggero = parseInt(prompt("qunati anni hai"))
 
 
+let prezzoTotaleBiglietto= (0,21 * numeroChilometri);
 
-
-const prezzoTotaleBiglietto= (0,21 * numeroChilometri);
-
-if (etàPasseggero > 18){
+if (etàPasseggero > 18)
+{
     ((prezzoTotaleBiglietto*40)/100)
-} else(etàPasseggero<18){
+} else (etàPasseggero<18)
+{
     ((prezzoTotaleBiglietto*20)/100)
 }
 
